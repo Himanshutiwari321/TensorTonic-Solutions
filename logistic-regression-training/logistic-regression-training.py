4e683da1abc323ec8fa78f94df8281717feddd4d
Himanshu Tiwari <himanshu546686@gmail.com>
2026-03-15 16:38:28 +0530

    Add solution for logistic-regression-training

---
+++ logistic-regression-training/logistic-regression-training.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def _sigmoid(z):
+    """Numerically stable sigmoid implementation."""
+    return np.where(z >= 0, 1/(1+np.exp(-z)), np.exp(z)/(1+np.exp(z)))
+
+def train_logistic_regression(X, y, lr=0.1, steps=1000):
+    """
+    Train logistic regression via gradient descent.
+    Return (w, b).
+    """
+    # Write code here
+    m, n = X.shape
+    
+    # initialize parameters
+    w = np.zeros(n)
+    b = 0
+
+    for _ in range(steps):
+        
+        # Linear combination
+        z = np.dot(X, w) + b
+        
+        # Prediction
+        y_hat = _sigmoid(z)
+
+        # Gradients
+        dw = (1/m) * np.dot(X.T, (y_hat - y))
+        db = (1/m) * np.sum(y_hat - y)
+
+        # Update
+        w -= lr * dw
+        b -= lr * db
+
+    return w, b
